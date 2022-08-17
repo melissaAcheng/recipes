81e4868391cb39d705db80fce2ec711dd18c89a2
melissaAcheng <melcheng12@gmail.com>
2022-08-17 10:56:20 -0400

    added bootstrap to registration and login pages

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,4 +1,4 @@
-from flask_app import app 
+from flask_app import app
 from flask import render_template, redirect, session, request
 from flask import flash
 from flask_app.models import user, recipe
@@ -6,15 +6,26 @@ from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 
 # localhost:5000 route root
+
+
 @app.route('/')
 def index():
 
     return render_template('index.html')
 
+# Login route
+
+
+@app.route('/signin')
+def signin():
+    return render_template('login.html')
+
 # Create Registration - POST request form data and IF INFO VALID redirect to dashboard
+
+
 @app.route('/register', methods=['POST'])
 def register():
-    # validate information 
+    # validate information
     # IF NOT then redirect to '/'
     if not user.User.validate_registration(request.form):
         return redirect('/')
@@ -23,8 +34,8 @@ def register():
     pw_hash = bcrypt.generate_password_hash(request.form['password'])
 
     data = {
-        "first_name":request.form["first_name"],
-        "last_name":request.form["last_name"],
+        "first_name": request.form["first_name"],
+        "last_name": request.form["last_name"],
         "email": request.form["email"],
         "password": pw_hash
     }
@@ -33,9 +44,11 @@ def register():
     user_id = user.User.register(data)
     # save in sessions
     session['user_id'] = user_id
-    return redirect ('/dashboard')
+    return redirect('/dashboard')
 
 # Login route
+
+
 @app.route('/login', methods=['POST'])
 def login():
     # check if the email provided exists in the database
@@ -44,16 +57,18 @@ def login():
     # if no email found
     if not user_in_db:
         flash("Invalid Login Credentials", "login_error")
-        return redirect('/')
+        return redirect('/signin')
     # if password does not match
     if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
         flash("Invalid Login Credentials", "login_error")
-        return redirect('/')
+        return redirect('/signin')
     # if login credentials match, set the user_id into session
     session['user_id'] = user_in_db.id
     return redirect('/dashboard')
 
 # GET dashboard
+
+
 @app.route('/dashboard')
 def display_dashboard():
     if 'user_id' not in session:
@@ -61,13 +76,15 @@ def display_dashboard():
     data = {
         'id': session['user_id']
     }
-    
+
     user_info = user.User.get_by_id(data)
     users_recipes = recipe.Recipe.get_all_recipes(data)
 
-    return render_template('dashboard.html', user = user_info, recipes = users_recipes)
+    return render_template('dashboard.html', user=user_info, recipes=users_recipes)
 
 # Logout
+
+
 @app.route('/logout')
 def logout():
     session.clear()
